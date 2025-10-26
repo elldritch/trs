b08588701c0fc4747e7ceea21b42e8a60f55e7aa
Eliza Zhang <eliza@elizazhang.com>
2025-10-26 02:22:23 -0700

    feat: Landing page and scaffolding

--- turbotreat/app/routes.ts
+++ turbotreat/app/routes.ts
@@ -1,3 +1,29 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  // All users land on this page when they first arrive. This is a marketing
+  // landing page, with a button link that sends you to `/application`.
+  index("routes/home.tsx"),
+  ...prefix("application", [
+    // When users first start their application, they land on this page. The
+    // component at this page reads `localStorage`, determines the current
+    // correct next step, and then redirects the user to the correct next
+    // `/steps/{step}`. This logic exists here in case a user panics and goes
+    // back to the landing page, so we can catch them and redirect them to steps
+    // already filled.
+    index("routes/start.tsx"),
+    // Each step's component also contains logic during initialization to read
+    // `localStorage`, determined the correct next step, and redirect the user
+    // if the correct next step is not the currently loaded step.
+    ...prefix("steps", [route("1", "routes/steps/1.tsx")]),
+    // At the very end, we present a review screen and submit the application to
+    // the database. We then clear the application state from `localStorage`,
+    // and the application can no longer be edited.
+    route("finish", "routes/submit.tsx"),
+  ]),
+] satisfies RouteConfig;
--- turbotreat/app/routes/home.tsx
+++ turbotreat/app/routes/home.tsx
@@ -1,13 +1,37 @@
+import { Link } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "TurboTreat®" },
-    { name: "description", content: "Welcome to TurboTreat, hosted by Arbor Halloween!" },
+    {
+      name: "description",
+      content: "Welcome to TurboTreat, hosted by Arbor Halloween!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <header className="h-8 px-4 mt-4">
+        <img src="/public/unintuit-turbotreat.png" className="h-full" />
+      </header>
+      <main className="p-4">
+        <div className="rounded-lg bg-stone-100 py-10 px-8 text-center">
+          <h1 className="text-2xl font-light">
+            Ready to file your 2025 treat return? TurboTreat can help you file.
+          </h1>
+          <p className="text-sm font-light mt-6">
+            Get every treat you deserve on your refund, guaranteed*
+          </p>
+          <Link to="/application">
+            <button className="mt-6 rounded-md bg-sky-700 font-medium text-lg text-white w-full py-2">
+              File my return
+            </button>
+          </Link>
+        </div>
+      </main>
+    </>
+  );
 }
