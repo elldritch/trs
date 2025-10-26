c8a61b539c2b4d9d561ca6fe79e24516fb89f045
Eliza Zhang <eliza@elizazhang.com>
2025-10-25 18:53:08 -0700

    feat: favicon and metadata

--- turbotreat/app/routes/home.tsx
+++ turbotreat/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "TurboTreat®" },
+    { name: "description", content: "Welcome to TurboTreat, hosted by Arbor Halloween!" },
   ];
 }
 
