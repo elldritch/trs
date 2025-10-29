96b53794c0b97b601b07858de8b284fdf8416487
Caitlin Hogan <cahogan16@gmail.com>
2025-10-28 22:33:46 -0400

    Show "return to review" button on step pages if readyToSubmit is set

--- turbotreat/app/routes/steps/1.tsx
+++ turbotreat/app/routes/steps/1.tsx
@@ -1,13 +1,14 @@
 import MultipleChoice from "app/components/multiple-choice";
 import { useAppState } from "~/contexts/ApplicationStateContext";
-import type { ApplicationState, QuestionAnswer } from "~/lib/types";
+import type { QuestionAnswer } from "~/lib/types";
 import { getCurrentQuestionAnswers } from "~/lib/state.client";
 import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router";
 
 export default function Step1() {
   // The below code will be used in every step, probably there is a
   // nicer way to decompose this
-  const { state } = useAppState() as { state: ApplicationState };
+  const { state, setReadyToSubmit } = useAppState();
   const [answers, setAnswers] = useState<QuestionAnswer[]>([]);
   useEffect(() => {
     setAnswers(getCurrentQuestionAnswers(state));
@@ -28,5 +29,20 @@ export default function Step1() {
     options={["Dog", "Cat", "Bird"]}
     answer={answerMap.get("q3")?.answer ?? ""}
     />
+
+    <div className="mt-6 flex gap-4">
+      {state.state === "in_progress" && state.readyToSubmit && (
+        <Link to="/application/finish">
+          <button className="rounded-md bg-stone-500 font-medium text-white px-6 py-2">
+            Return to Review
+          </button>
+        </Link>
+      )}
+      <Link to="/application/finish" onClick={() => setReadyToSubmit(true)}>
+        <button className="rounded-md bg-sky-700 font-medium text-white px-6 py-2">
+          Continue to Review
+        </button>
+      </Link>
+    </div>
   </div>;
 }
