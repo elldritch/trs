63bed4341746e1abf901b8d7cf62a821f62a691d
Eliza Zhang <eliza@elizazhang.com>
2025-10-29 03:46:30 -0700

    refactor: Clean up database schema

--- trs-db/index.ts
+++ trs-db/index.ts
@@ -1,8 +1,3 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "./generated/prisma/client";
 
-export * from '@prisma/client'
-
-// Note: I am not sure if this is the best practice for
-// using the data model across both apps, so don't hesitate
-// to change it!
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient();
