c2c875f0d178cb8e2398aa0675f855fcceed33ec
Caitlin Hogan <cahogan16@gmail.com>
2025-10-30 23:44:08 -0700

    Use TRS logo for admin app

--- trs-admin/app/components/AdminNavbar.tsx
+++ trs-admin/app/components/AdminNavbar.tsx
@@ -4,8 +4,7 @@ export default function AdminNavbar() {
     return (
         <header className="h-18 py-4 bg-trs-blue flex items-center justify-between px-6">
             <div className="flex items-center">
-                {/* <img src="/trs-logo.png" className="h-14 inline-block" /> */}
-                <Link to="/"><p className="inline-block text-white font-serif text-4xl">TRS</p></Link>
+            <Link to="/"><img src="/trs-logo-white.png" className="h-10 inline-block" /></Link>
             </div>
             <div className="flex items-center gap-4">
                 <p className="text-white text-xl">SEARCH</p>
