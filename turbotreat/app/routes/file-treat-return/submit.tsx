feba13f473ffd1f242717ff570f90c382fbb8683
Eliza Zhang <eliza@elizazhang.com>
2025-10-31 11:41:53 -0700

    feat: More aggressive warning for return IDs

--- turbotreat/app/routes/file-treat-return/submit.tsx
+++ turbotreat/app/routes/file-treat-return/submit.tsx
@@ -184,6 +184,7 @@ export default function Submit({ loaderData }: Route.ComponentProps) {
           <div className="mt-4 text-center flex flex-col gap-4">
             <p>Your treat return ID is:</p>
             <h2 className="text-4xl font-light">{fetcher.data.ticketId}</h2>
+            <p>WRITE DOWN THIS RETURN ID. IT WILL NOT BE SHOWN TO YOU AGAIN.</p>
             <p>
               Your return can be downloaded at{" "}
               <a
